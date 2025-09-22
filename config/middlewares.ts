2261dbc227aca46f11cfd14c2779b60e4ebdd0eb
Armen <front2@greca.co>
2025-09-22 21:26:46 +0400

    first commit

---
+++ config/middlewares.ts
@@ -0,0 +1,40 @@
+module.exports = [
+  "strapi::errors",
+  /* Replace 'strapi::security', with this snippet */
+  /* Beginning of snippet */
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "shamsmedia.s3.us-east-1.amazonaws.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "shamsmedia.s3.us-east-1.amazonaws.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  /* End of snippet */
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
